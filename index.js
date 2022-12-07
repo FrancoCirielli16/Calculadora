b7e7cb2355d40bc84c4249e7a7adfe415523bb3a
Franco Cirielli <ciriellifrancogiovanni@gmail.com>
2022-12-07 14:31:09 -0300

    v3

--- index.js
+++ index.js
@@ -4,6 +4,25 @@ const displayValorActual = document.getElementById('valor-actual');
 const botonesNumeros = document.querySelectorAll('.numero');
 const botonesOperadores = document.querySelectorAll('.operador'); 
 
+
+const preferedColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+const slider = document.getElementById('slider');
+
+const setTheme = (theme) => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+}
+
+slider.addEventListener('click', () => {
+    let switchToTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
+    setTheme(switchToTheme);
+});
+
+setTheme(localStorage.getItem('theme') || preferedColorScheme);
+
+
+
+
 const display = new Display(displayValorAnterior,displayValorActual);
 
 botonesNumeros.forEach(boton => {
@@ -13,3 +32,4 @@ botonesNumeros.forEach(boton => {
 botonesOperadores.forEach(boton => {
     boton.addEventListener('click', () => display.computar(boton.value));
 });
+
